28d449ec111004222a2e9a0e914b425854238c6a
Oskar Pietrucha <velho.oscar@outlook.com>
2023-04-26 18:45:32 +0200

    minor fixes, text changes and added package

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div>
       <Link to="/about">About</Link>
-      <h1>Thanks for supporting me</h1>
+      <h1>Thanks visiting the page</h1>
       <h2>Github Pages</h2>
       <h3>Deploying React to Github</h3>
     </div>
